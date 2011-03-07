74071695f8102fa56e1b2dfa2b29ed6afc457a6e
juraj.janecek <juraj.janecek@c7bccf14-faac-419c-8785-6bd0ae538edf>
2011-03-07 11:13:49 +0000

    Fixed problem with uknown numbers (clear calls)

--- src/jjsan/widget/callback/ClickOneActivity.java
+++ src/jjsan/widget/callback/ClickOneActivity.java
@@ -10,33 +10,35 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.ContactsContract;
 import android.provider.ContactsContract.PhoneLookup;
 import android.util.Log;
-import android.widget.Toast;
 
 public class ClickOneActivity extends Activity {
 
-	public String callerPhoneNumber = null;
-	public String callerName = null;
-	
+	public String callerPhoneNumber = "no_callerPhoneNumber";
+	public String callerName = "no_callerName";
+	private AlertDialog.Builder alertbox;	
 	@Override
+		
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
-			
+
 			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());   
 			String showDialog = prefs.getString("show_dialog","true");
-			
+
 		    //we need last number
-			//Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
 			// Querying for a cursor is like querying for any SQL-Database
-			Cursor c = getContentResolver().query(
+			Cursor c = null;
+			try	{
+			c = getContentResolver().query(
 					android.provider.CallLog.Calls.CONTENT_URI,
 					null, null, null, 
 					android.provider.CallLog.Calls.DATE + " DESC");
+					startManagingCursor(c);
+			}
+			catch(Exception e) { ; 
+			}
 
-			startManagingCursor(c);
-			
 			try	{ 
 				boolean moveToFirst=c.moveToFirst(); 
 				if (moveToFirst) {};
@@ -46,35 +48,78 @@ public class ClickOneActivity extends Activity {
 			// could not move to the first row. return; 
 			//Toast.makeText(getApplicationContext(), "Error moving to first ", Toast.LENGTH_SHORT).show();
 			}
-
-			int numberColumn = c.getColumnIndex(
+			
+			int numberColumn = 0;
+			try	{
+					numberColumn = c.getColumnIndex(
 					android.provider.CallLog.Calls.NUMBER);
-
-			//just first number
+					//just first number
+			}
+			catch(Exception e) { ; 
+			// could not move to the first row. return; 
+			//Toast.makeText(getApplicationContext(), "Error moving to first ", Toast.LENGTH_SHORT).show();
+			}
+			
+			try	{
 			if(c.isFirst()){
 				//do{
 					callerPhoneNumber = c.getString(numberColumn);
 				//}while(c.isLast());
 			}
+			}
+			catch(Exception e) { ; 
+			// could not move to the first row. return; 
+			}
 			
+			try	{
 			c = getContentResolver().query(Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(callerPhoneNumber)), new String[] {PhoneLookup.DISPLAY_NAME}, null, null, null);
 	         while(c.moveToNext()){
 	        	 callerName = c.getString(c.getColumnIndexOrThrow(PhoneLookup.DISPLAY_NAME));
 	         }
-	         
+			}
+			catch(Exception e) { ; 
+			// could not move to the first row. return; 
+			}
+
+	        
 			//Toast.makeText(getApplicationContext(), "Alert dialog", Toast.LENGTH_SHORT).show();
+			//Toast.makeText(getApplicationContext(), callerName, Toast.LENGTH_SHORT).show();
+			//Toast.makeText(getApplicationContext(), callerPhoneNumber, Toast.LENGTH_SHORT).show();
+			//do we have a number?
+			if (callerName.equalsIgnoreCase("no_callerName")&&callerPhoneNumber.equalsIgnoreCase("-1")) 
+			{
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setMessage(getString(R.string.noPhoneNumber))
+				       .setCancelable(false)
+				       .setPositiveButton(getString(R.string.strOK), new DialogInterface.OnClickListener() {
+				           public void onClick(DialogInterface dialog, int id) {
+				                finish();
+				           }
+				       });
+				
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+
+			
 			if (showDialog.equalsIgnoreCase("true")) {
 			//ask a question ;)
-			AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+			
+			alertbox = new AlertDialog.Builder(this);
+			
+			
             // Set the message to display
-            if (callerName.equalsIgnoreCase("null")) {
+            if (callerName.equalsIgnoreCase("no_callerName")) {
             	alertbox.setMessage(getString(R.string.qeuCallBack)+"\n"+callerPhoneNumber+" ?");
             }	
             else
             {
-            	alertbox.setMessage(getString(R.string.qeuCallBack)+"\n"+callerName+"?");	
+            	if (!callerName.equalsIgnoreCase("no_callerName"))
+            	alertbox.setMessage(getString(R.string.qeuCallBack)+"\n"+callerName+"?");
             }
-             // Set a positive/yes button and create a listener
+			
+            
+            // Set a positive/yes button and create a listener
             alertbox.setPositiveButton(getString(R.string.strYes), new DialogInterface.OnClickListener() {
                 // Click listener 
                 public void onClick(DialogInterface arg0, int arg1) {
@@ -92,6 +137,7 @@ public class ClickOneActivity extends Activity {
         		    finish();
                 }
             });
+            
             // Set a negative/no button and create a listener
             alertbox.setNegativeButton(getString(R.string.strNo), new DialogInterface.OnClickListener() {
                 // Click listener
@@ -101,7 +147,10 @@ public class ClickOneActivity extends Activity {
                 	finish();
                 }
             });
-            alertbox.show();
+            if (!callerName.equalsIgnoreCase("no_callerName")&&!callerPhoneNumber.equalsIgnoreCase("-1"))
+            {
+            	alertbox.show();
+			}
 									}
 			else
 			{
@@ -118,7 +167,5 @@ public class ClickOneActivity extends Activity {
     		    finish();
 			}
 			
-            // display box
-			//Toast.makeText(getApplicationContext(), "Display Alert dialog", Toast.LENGTH_SHORT).show();
 		}
 }
